4e9c998825112c9b8456230c60ae29563f6e4b61
leeysi <yeseul726@e-mirim.hs.kr>
2017-05-23 20:57:53 +0900

    #3 Make Context menu and register

--- app/src/main/java/yeseul/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -2,6 +2,10 @@ package yeseul.kr.hs.emirim.contextmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but1 = (Button)findViewById(R.id.but1);
+        Button but2 = (Button)findViewById(R.id.but2);
+        registerForContextMenu(but1);
+        registerForContextMenu(but2);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        if(v.getId() == R.id.but1){
+            menu.setHeaderTitle("배경색 변경");
+            //menu.setHeaderIcon(R.drawable.icon); //아이콘 넣는 방법
+            menuInflater.inflate(R.menu.menu1, menu);
+        }
+        if(v.getId()==R.id.but2){
+            menuInflater.inflate(R.menu.menu2, menu);
+        }
     }
 }
